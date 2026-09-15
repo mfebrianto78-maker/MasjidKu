5a459b17e325703ca1a7de5396957bf2f217e676
mfebrianto78-maker <mfebrianto78@gmail.com>
2026-09-15 13:13:30 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'masjidku-v1'
-const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.webmanifest']
+const CACHE_NAME = 'masjidku-v2'
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png']
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)))
@@ -10,12 +10,12 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   )
 })
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   )
 })
